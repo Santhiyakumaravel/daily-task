6f15d22ade54fdf9f17623601a723530f287a083
Santhiyakumaravel <santhiyaaiml@gmail.com>
2026-03-19 16:58:45 +0530

    Add files via upload

---
+++ day4/duplicate.java
@@ -0,0 +1,25 @@
+package day4;
+import java.util.Arrays;
+import java.util.Scanner;
+public class duplicate  {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int[]arr=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<a;i++)
+        {
+            for(int j=i+1;j<a;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    System.out.print(arr[i]);
+                }
+            }
+        }
+    }
+}
